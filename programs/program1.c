3c43807eb6744c3f62ed7637cd6e4a3afbad5b19
PrathviiXD <prathvirajsinghsushma@gmail.com>
2026-04-15 22:48:57 +0530

    Day1

---
+++ programs/program1.c
@@ -0,0 +1,29 @@
+// #include <stdio.h>
+
+// int main(){
+
+//     // DO YOUR HOMEWORK
+
+//     /*
+//     I
+//     WILL
+//     KNOW
+//     IF
+//     YOU
+//     DON'T
+//     */
+
+//     printf("I like pizza!\n");
+//     printf("It's really good!");
+
+//     return 0;
+// }
+
+#include <stdio.h>
+int main() {
+    // // having fun today!!!
+    // Why? = because i like watermelon so much!!
+    printf("I Love Watermelon Soo Much!! \n ");
+    printf("I Really Love it!");
+    return 0;
+}
