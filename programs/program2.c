c08ed77c0d4964106e8c5a5660ee22d1b0827982
PrathviiXD <prathvirajsinghsushma@gmail.com>
2026-04-17 17:44:02 +0530

    Here we goo!!

--- programs/program2.c
+++ programs/program2.c
@@ -66,3 +66,15 @@
 //     return 0;
 // }
 
+
+#include <stdio.h>
+
+
+int main() {
+
+    int n;
+     printf("enter the value: \n") ;
+     scanf("%d",&n);
+     printf("datatype: %d");
+      return 0;
+}
---
+++ programs/program3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    int num1=1;int num2=10;int num3=100;
+    printf("%3d\n%6d\n%8d\n", num1, num2, num3);
+
+    bool Absent=1;
+    if(Absent){printf("Nalayak Baccha\n");}else{printf("DevManus Baccha\n");}
+
+    int num[5]={42,68,59,65,44,56};
+    printf("%d", num[4]);
+
+    
+    return 0;
+
+    
+}
